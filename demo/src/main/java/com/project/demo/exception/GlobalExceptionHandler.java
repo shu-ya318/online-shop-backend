87d63b8f8c415f0e66676d757ea66f80bbe3a263
shu-ya318 <funghi0983524367@gmail.com>
2025-09-06 14:59:10 +0800

    fix(api): handle missing or invalid uuid path param with 404 response

--- demo/src/main/java/com/project/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/project/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,20 @@
 package com.project.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String message = String.format("The UUID is invalid: %s", ex.getValue());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", message));
+    }
 }
