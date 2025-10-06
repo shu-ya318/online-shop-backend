a81e143bebed369435836830a91379f7ff3e7755
shu-ya318 <funghi0983524367@gmail.com>
2025-10-06 12:23:54 +0800

    build(docker): add Docker support and update project configs

--- demo/src/main/java/com/project/demo/DemoApplication.java
+++ demo/src/main/java/com/project/demo/DemoApplication.java
@@ -2,14 +2,20 @@ package com.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 }
